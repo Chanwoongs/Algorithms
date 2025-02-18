b2a2da6e0147e257ba1184d1f51ec83de8dc8b83
Chanwoong Bang <bck4044@gmail.com>
2025-02-18 16:41:17 +0900

    feat: 데이터를 파일 입출력을 통해 미리 받아오기

--- Algorithm/9.RedBlackTree/main.cpp
+++ Algorithm/9.RedBlackTree/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <vector>
 
 #include "RedBlackTree.h"
 #include "Windows.h"
@@ -158,12 +159,80 @@ void ProcessCommand(int commandInput, RedBlackTree& tree)
     }
 }
 
+void Initialization(const char* path, RedBlackTree* tree)
+{
+    if (tree == nullptr)
+    {
+        std::cout << "오류: 전달된 트리가 null 입니다.\n";
+        return;
+    }
+
+    FILE* file = nullptr;
+    fopen_s(&file, path, "r");
+    if (file == nullptr)
+    {
+        std::cout << "오류: 파일 읽기 실패.\n";
+        return;
+    }
+
+    char line[1024] = { };
+    if (!fgets(line, 1024, file))
+    {
+        std::cout << "오류: 파일 읽기 실패.\n";
+        fclose(file);
+        return;
+    }
+
+    fclose(file);
+
+    std::vector<int> datas;
+
+    // 첫 토근 읽기.
+    char* context = nullptr;
+    char* splitString = strtok_s(line, ",", &context);
+    if (splitString)
+    {
+        if (int parsed = atoi(splitString))
+        {
+            datas.emplace_back(parsed);
+        }
+    }
+
+    // 나머지 토근 읽기.
+    while (context)
+    {
+        splitString = strtok_s(nullptr, ",", &context);
+        if (!splitString)
+        {
+            break;
+        }
+
+        if (int parsed = atoi(splitString))
+        {
+            datas.emplace_back(parsed);
+        }
+    }
+
+    // 트리에 데이터 추가.
+    for (auto data : datas)
+    {
+        tree->Insert(data);
+    }
+
+    // 초기 데이터를 설정한 후의 데이터 출력.
+    std::cout << "초기 데이터가 적용되었습니다.\n";
+    tree->Print();
+    std::cout << "\n";
+}
+
 int main()
 {
     _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 
     // 레드 블랙 트리 생성.
     RedBlackTree tree;
+
+    Initialization("../Assets/InitData.txt", &tree);
 
     while (true)
     {
